d6db07180c5006676a85a64d9d847ec94a4722aa
Alex-Merrill <aomerrill98@gmail.com>
2020-10-26 02:43:14 -0400

    adds basic movement. starts working on attack

--- godotPlayground/Scripts/Player.gd
+++ godotPlayground/Scripts/Player.gd
@@ -1,55 +1,90 @@
 extends Actor
 
+onready var platform_detector = $PlatformDetector
 
 # Declare member variables here. Examples:
-var jumpvelocity = -150
-var gravityscale = 1
-var walkspeed = 500
+const JUMP_FORCE = -120
+const SPEED = 10
+const GRAVITY = Vector2(0, 15);
+const MAX_VELOCITY = Vector2(1200, 1200)
 
-# 0 is left, 1 is right
-var direction = Vector2.ZERO
+# physics
+var acceleration = Vector2.ZERO
+var velocity = Vector2.ZERO
+var direction = Vector2.ZERO # 0 is left, 1 is right
+
+var isAttacking = false;
+
+
 var form = "Skeleton"
+
 
 # Called when the node enters the scene tree for the first time.\
 func _ready():
 	pass # Replace with function body.
 
 func get_input():
-	if Input.is_action_pressed("ui_left"):
+
+	if Input.is_action_pressed("my_left"): # move left
 		direction.x = -1
-		$Skeleton.play("Walk")
+		apply_force(Vector2(-SPEED, 0))
+		$Skeleton.flip_h = true
+		if !isAttacking: $Skeleton.play("Walk")
 		
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("my_right"): # move right
 		direction.x = 1
-		$Skeleton.play("Walk")
-		
-	elif Input.is_action_pressed("ui_accept"):
-		pass
-		
-	else:
-		$Skeleton.play("Idle")
-		direction.x = 0
-		
-		
+		apply_force(Vector2(SPEED, 0))
+		$Skeleton.flip_h = false
+		if !isAttacking: $Skeleton.play("Walk")
+
+	if Input.is_action_pressed("my_jump"): # jump
+		if platform_detector.is_colliding():
+			apply_force(Vector2(0, JUMP_FORCE))
+	
+	if Input.is_action_pressed("my_attack"): # attack
+		if !isAttacking:
+			isAttacking = true
+			$Skeleton.play("Attack")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-func calculate_move_velocity():
-	return speed * direction
-	
+
+
+#func calculate_move_velocity(speed):
+#	return WALKSPEED * velocity
+
+
 func _physics_process(delta):
+	# handles input
 	get_input()
 
-		
-	#velocity.y += gravity * delta
+	# applies gravity and updates velocity
+	apply_force(GRAVITY)
+	velocity += acceleration
+
+
+	# applies movement
+	move_and_slide(velocity)
+
+	# resets acceleration and applies a sort of friction
+	acceleration *= Vector2.ZERO
+	velocity.x = lerp(0, velocity.x, .98)
 	
-	if direction.x < 0:
-		$Skeleton.flip_h = true
-	elif direction.x > 0:
-		$Skeleton.flip_h = false
-	var velocity = calculate_move_velocity()
-	#velocity.y = 2
-	move_and_collide(velocity)
-	
+	# double checks animation
+	if abs(velocity.x) < 10 && !isAttacking:
+		$Skeleton.play("Idle")
+		direction.x = 0
+	elif abs(velocity.x) > 10 && !isAttacking:
+		$Skeleton.play("Walk")
+
+
+# applies a force to the player
+func apply_force(force):
+	acceleration += force;
+
+
+func _on_Skeleton_animation_finished():
+		if isAttacking:
+			isAttacking = false
