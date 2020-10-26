672c805e4900f8ec99d93d71958cf905ebf5e051
Scott Powell <smpowell@middlebury.edu>
2020-10-26 00:25:27 -0400

    Player passes through tilemap, despite collision indicating it shouldn't...

--- godotPlayground/Scripts/Actor.gd
+++ godotPlayground/Scripts/Actor.gd
@@ -6,12 +6,13 @@ extends KinematicBody2D
 export var speed = Vector2(5.0, 350.0)
 onready var gravity = ProjectSettings.get("physics/2d/default_gravity")
 
-
+var FLOOR_NORMAL = Vector2.UP
+var _velocity = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	_velocity.y += gravity * delta
--- godotPlayground/Scripts/Player.gd
+++ godotPlayground/Scripts/Player.gd
@@ -5,11 +5,10 @@ extends Actor
 var jumpvelocity = -150
 var gravityscale = 1
 var walkspeed = 500
-
+var FLOOR_DETECT_DISTANCE = 20
 # 0 is left, 1 is right
-var direction = Vector2.ZERO
+var direction = Vector2.ONE
 var form = "Skeleton"
-
 # Called when the node enters the scene tree for the first time.\
 func _ready():
 	pass # Replace with function body.
@@ -29,8 +28,6 @@ func get_input():
 	else:
 		$Skeleton.play("Idle")
 		direction.x = 0
-		
-		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -42,14 +39,19 @@ func calculate_move_velocity():
 func _physics_process(delta):
 	get_input()
 
-		
+	var snap_vector = Vector2.DOWN * FLOOR_DETECT_DISTANCE if direction.y == 0.0 else Vector2.ZERO
+	var velocity = calculate_move_velocity()
 	#velocity.y += gravity * delta
+	var is_on_platform = $PlatformDetector.is_colliding()
+	print(is_on_platform)
+	velocity = move_and_slide_with_snap(
+		velocity, snap_vector, FLOOR_NORMAL, not is_on_platform, 4, 0.9, false
+	)
 	
 	if direction.x < 0:
 		$Skeleton.flip_h = true
 	elif direction.x > 0:
 		$Skeleton.flip_h = false
-	var velocity = calculate_move_velocity()
-	#velocity.y = 2
-	move_and_collide(velocity)
+	var collision = move_and_slide(velocity)
+	#print(collision)
 	
