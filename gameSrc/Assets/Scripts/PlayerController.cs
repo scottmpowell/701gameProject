8a94d764dc37feea33c4a990fe5524864fc697ec
Alex-Merrill <aomerrill98@gmail.com>
2020-10-27 23:39:46 -0400

    movement is actually not half-bad

--- gameSrc/Assets/Scripts/PlayerController.cs
+++ gameSrc/Assets/Scripts/PlayerController.cs
@@ -2,24 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Spine.Unity;
-using TMPro;
 
 public class PlayerController : MonoBehaviour {
 
+    //animation stuff
     public SkeletonAnimation skeletonAnimation;
-    public AnimationReferenceAsset idle, walk, jump;
+    public AnimationReferenceAsset idle, walk, jump, run;
     public string currentState;
     public string currentAnimation;
-    public float max_speed;
-    public float movement_scalar;
-    public float jump_scalar;
-    private bool is_on_ground;
+
+
+    // physics stuff
     private Rigidbody2D rigidBody;
+    public float acceleration;
+    public float deceleration;
+    public float maxSpeed;
+    public float jumpSpeed;
+    private bool isJumping;
+    private bool isGrounded;
+
 
     // Start is called before the first frame update
     void Start() {
         rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.constraints = RigidbodyConstraints2D.FreezeRotation; // prevents sprite from rotating
         currentState = "Idle";
+        isJumping = false;
         SetCharacterState(currentState);
     }
 
@@ -28,57 +36,125 @@ public class PlayerController : MonoBehaviour {
         Move();
     }
 
-    public void SetAnimation(AnimationReferenceAsset animation, bool loop, float timeScale) {
+    public void Move()
+    {
+        // keeps track of new velocity to set as rigidBody velocity
+        Vector2 movement = Vector2.zero;
 
-        if(animation.name.Equals(currentAnimation)) {
+        // Handles walking physics
+        float x_input = Input.GetAxis("Horizontal");
+        if (x_input > 0) // handles acceleration
+        {
+            movement.x = rigidBody.velocity.x + maxSpeed * Time.deltaTime * acceleration;
+            transform.localScale = new Vector2(1f, 1f); // flips sprite
+        }
+        else if (x_input < 0)
+        {
+            movement.x = rigidBody.velocity.x + -maxSpeed * Time.deltaTime * acceleration;
+            transform.localScale = new Vector2(-1f, 1f); // flips sprite
+        }
+        else // handles deceleration
+        {
+            if (rigidBody.velocity.x > 0)
+            {
+                movement.x = rigidBody.velocity.x - maxSpeed * Time.deltaTime * deceleration;
+                if (movement.x < 0)
+                {
+                    movement.x = 0;
+                }
+            }
+            else if (rigidBody.velocity.x < 0)
+            {
+                movement.x = rigidBody.velocity.x + maxSpeed * Time.deltaTime * deceleration;
+                if(movement.x > 0)
+                {
+                    movement.x = 0;
+                }
+            }
+            else
+            {
+                movement.x = 0;
+            }
+        }
+
+        // caps velocity by maxSpeed
+        if (movement.x > maxSpeed)
+        {
+            movement.x = maxSpeed;
+        } else if (movement.x < -maxSpeed)
+        {
+            movement.x = -maxSpeed;
+        }
+
+        // handles jumping
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            isJumping = true;
+            movement.y = jumpSpeed;
+        } 
+        else
+        {
+            movement.y = rigidBody.velocity.y;
+        }
+
+        // handles some animation
+        if ((movement.x > 8 || movement.x < -8) && !isJumping && isGrounded)
+        {
+            SetCharacterState("Run");
+        }
+        else if (movement.x != 0 && !isJumping && isGrounded)
+        {
+            SetCharacterState("Walk");
+        } 
+        else if (movement.x == 0 && !isJumping && isGrounded)
+        {
+            SetCharacterState("Idle");
+        } else if(isJumping && isGrounded)
+        {
+            SetCharacterState("Jump");
+        }
+         
+        // applies new velocity to player Rigidbody2D
+        rigidBody.velocity = movement;
+
+    }
+
+    public void SetAnimation(AnimationReferenceAsset animation, bool loop, float timeScale)
+    {
+
+        if (animation.name.Equals(currentAnimation))
+        {
             return;
         }
         skeletonAnimation.state.SetAnimation(0, animation, loop).TimeScale = timeScale;
         currentAnimation = animation.name;
     }
 
-    public void SetCharacterState(string state) {
-        if(state.Equals("Idle")) {
-            SetAnimation(idle, true, 1f);
-        } else if (state.Equals("Walk")) {
-            SetAnimation(walk, true, 2f);
-        } else if (state.Equals("Jump")) {
-            SetAnimation(jump, true, 1f);
-        }
-    }
-
-    public void Move() {
-        Vector2 movement = Vector2.zero;
-
-        if((rigidBody.velocity.x < max_speed && rigidBody.velocity.x >= 0) || Input.GetAxis("Horizontal") < 0)
-        { 
-            movement.x = Input.GetAxis("Horizontal");
-        } else if ((rigidBody.velocity.x > -max_speed && rigidBody.velocity.x <= 0) || Input.GetAxis("Horizontal") > 0)
+    public void SetCharacterState(string state)
+    {
+        if (state.Equals("Idle"))
         {
-            movement.x = Input.GetAxis("Horizontal");
+            SetAnimation(idle, true, 1f);
         }
-        if(Input.GetButtonDown("Jump") && is_on_ground) {
-            movement.y = jump_scalar;
+        else if (state.Equals("Walk"))
+        {
+            SetAnimation(walk, true, 1f);
         }
-        rigidBody.AddForce(movement_scalar * movement);
-
-        if (movement.x != 0) {
-            if (movement.x > 0) transform.localScale = new Vector2(1f, 1f);
-            if (movement.x < 0) transform.localScale = new Vector2(-1f, 1f);
-            SetCharacterState("Walk");
-        } else if (movement.y != 0) {
-            SetCharacterState("Jump");
-        } else {
-            SetCharacterState("Idle");
+        else if (state.Equals("Run"))
+        {
+            SetAnimation(run, true, 1f);
+        }
+        else if (state.Equals("Jump"))
+        {
+            SetAnimation(jump, false, .6f);
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision);
         if(CollisionIsWithGround(collision))
         {
-            is_on_ground = true;
+            isGrounded = true;
         }
     }
 
@@ -86,7 +162,8 @@ public class PlayerController : MonoBehaviour {
     {
         if(!CollisionIsWithGround(collision))
         {
-            is_on_ground = false;
+            isJumping = false;
+            isGrounded = false;
         }
     }
 
