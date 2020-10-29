8f7d4a2ae1e29c4d9552c3883246c5b5d8501531
Alex-Merrill <aomerrill98@gmail.com>
2020-10-29 01:13:09 -0400

    Adds new unity project, did minor changes in old unity project

---
+++ Final Game/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform followTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(followTransform.position.x + 4, followTransform.position.y + 1, -3);
+    }
+}
--- Final Game/Assets/Scripts/Player/PlayerController.cs
+++ Final Game/Assets/Scripts/Player/PlayerController.cs
@@ -1,44 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Spine.Unity;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    //animation stuff
-    public SkeletonAnimation skeletonAnimation;
-    public AnimationReferenceAsset idle, walk, jump, run;
-    public string currentState;
-    public string currentAnimation;
+    // animation stuff
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
-
-    // physics stuff
+    //phsyics stuff
     private Rigidbody2D rigidBody;
     public float acceleration;
     public float deceleration;
     public float maxSpeed;
     public float jumpSpeed;
-    private bool isJumping;
-    private bool isGrounded;
     private float dt;
-
+    private bool isGrounded;
+    //do jump enumeration shit
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
+        // get rigidbody component and freeze rotation on it
         rigidBody = GetComponent<Rigidbody2D>();
-        rigidBody.constraints = RigidbodyConstraints2D.FreezeRotation; // prevents sprite from rotating
-        currentState = "Idle";
+        rigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
         dt = Time.fixedDeltaTime;
-        isJumping = false;
-        SetCharacterState(currentState);
+
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         Move();
     }
 
-    public void Move()
+
+    void Move()
     {
         // keeps track of new velocity to set as rigidBody velocity
         Vector2 movement = Vector2.zero;
@@ -48,12 +50,12 @@ public class PlayerController : MonoBehaviour {
         if (x_input > 0) // handles acceleration
         {
             movement.x = rigidBody.velocity.x + maxSpeed * dt * acceleration;
-            transform.localScale = new Vector2(1f, 1f); // flips sprite
+            spriteRenderer.flipX = false; // flips sprite
         }
         else if (x_input < 0)
         {
             movement.x = rigidBody.velocity.x + -maxSpeed * dt * acceleration;
-            transform.localScale = new Vector2(-1f, 1f); // flips sprite
+            spriteRenderer.flipX = true; // flips sprite
         }
         else // handles deceleration
         {
@@ -68,7 +70,7 @@ public class PlayerController : MonoBehaviour {
             else if (rigidBody.velocity.x < 0)
             {
                 movement.x = rigidBody.velocity.x + maxSpeed * dt * deceleration;
-                if(movement.x > 0)
+                if (movement.x > 0)
                 {
                     movement.x = 0;
                 }
@@ -83,7 +85,7 @@ public class PlayerController : MonoBehaviour {
         if (movement.x > maxSpeed)
         {
             movement.x = maxSpeed;
-        } 
+        }
         else if (movement.x < -maxSpeed)
         {
             movement.x = -maxSpeed;
@@ -92,69 +94,23 @@ public class PlayerController : MonoBehaviour {
         // handles jumping
         if (Input.GetButton("Jump") && isGrounded)
         {
-            isJumping = true;
             movement.y = jumpSpeed;
-        } 
+        }
         else
         {
             movement.y = rigidBody.velocity.y;
         }
 
-        // handles some animation
-        if ((movement.x > 8 || movement.x < -8) && !isJumping && isGrounded)
-        {
-            SetCharacterState("Run");
-        }
-        else if (movement.x != 0 && !isJumping && isGrounded)
-        {
-            SetCharacterState("Walk");
-        } 
-        else if (movement.x == 0 && !isJumping && isGrounded)
-        {
-            SetCharacterState("Idle");
-        } else if(isJumping && isGrounded)
-        {
-            SetCharacterState("Jump");
-        }
-         
+        animator.SetBool("Grounded", isGrounded);
+        animator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
         // applies new velocity to player Rigidbody2D
         rigidBody.velocity = movement;
     }
 
-    public void SetAnimation(AnimationReferenceAsset animation, bool loop, float timeScale)
-    {
-
-        if (animation.name.Equals(currentAnimation))
-        {
-            return;
-        }
-        skeletonAnimation.state.SetAnimation(0, animation, loop).TimeScale = timeScale;
-        currentAnimation = animation.name;
-    }
-
-    public void SetCharacterState(string state)
-    {
-        if (state.Equals("Idle"))
-        {
-            SetAnimation(idle, true, 1f);
-        }
-        else if (state.Equals("Walk"))
-        {
-            SetAnimation(walk, true, 1f);
-        }
-        else if (state.Equals("Run"))
-        {
-            SetAnimation(run, true, 1f);
-        }
-        else if (state.Equals("Jump"))
-        {
-            SetAnimation(jump, false, .6f);
-        }
-    }
-
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(CollisionIsWithGround(collision))
+        if (CollisionIsWithGround(collision))
         {
             isGrounded = true;
         }
@@ -162,9 +118,8 @@ public class PlayerController : MonoBehaviour {
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if(!CollisionIsWithGround(collision))
+        if (!CollisionIsWithGround(collision))
         {
-            isJumping = false;
             isGrounded = false;
         }
     }
@@ -172,10 +127,10 @@ public class PlayerController : MonoBehaviour {
     private bool CollisionIsWithGround(Collision2D collision)
     {
         bool is_with_ground = false;
-        foreach(ContactPoint2D c in collision.contacts)
+        foreach (ContactPoint2D c in collision.contacts)
         {
             Vector2 collision_direction_vector = c.point - rigidBody.position;
-            if(collision_direction_vector.y < 0)
+            if (collision_direction_vector.y < 0)
             {
                 is_with_ground = true;
             }
